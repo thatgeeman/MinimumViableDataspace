f306b22ec585bea5f52e7212e1f87cb84affa08a
Geevarghese George <geevarghese.george@avl.com>
2025-11-10 16:08:23 +0100

    adapt for rootless podman

--- seed-k8s.sh
+++ seed-k8s.sh
@@ -17,11 +17,16 @@
 ## This script must be executed when running the dataspace from IntelliJ. Neglecting to do that will render the connectors
 ## inoperable!
 
+# had to change the ports due to rootless podman and now adjust it here.
+HOST_HTTP_PORT=8080
+HOST_HTTPS_PORT=1443
+echo "Using $HOST_HTTP_PORT (HTTP) and $HOST_HTTPS_PORT (TCP) for rootless podman"
+
 ## Seed application DATA to both connectors
 echo
 echo
 echo "Seed data to 'provider-qna' and 'provider-manufacturing'"
-for url in 'http://127.0.0.1/provider-manufacturing/cp' 'http://127.0.0.1/provider-qna/cp'
+for url in "http://127.0.0.1:$HOST_HTTP_PORT/provider-manufacturing/cp" "http://127.0.0.1:$HOST_HTTP_PORT/provider-qna/cp"
 do
   newman run \
     --folder "Seed" \
@@ -35,7 +40,7 @@ echo
 echo "Create linked assets on the Catalog Server"
 newman run \
   --folder "Seed Catalog Server" \
-  --env-var "HOST=http://127.0.0.1/provider-catalog-server/cp" \
+  --env-var "HOST=http://127.0.0.1:$HOST_HTTP_PORT/provider-catalog-server/cp" \
   --env-var "PROVIDER_QNA_DSP_URL=http://provider-qna-controlplane:8082" \
   --env-var "PROVIDER_MF_DSP_URL=http://provider-manufacturing-controlplane:8082" \
   ./deployment/postman/MVD.postman_collection.json
@@ -75,7 +80,7 @@ DATA_CONSUMER=$(jq -n --arg url "$CONSUMER_CONTROLPLANE_SERVICE_URL" --arg ihurl
            }
        }')
 
-curl --location "http://127.0.0.1/consumer/cs/api/identity/v1alpha/participants/" \
+curl --location "http://127.0.0.1:$HOST_HTTP_PORT/consumer/cs/api/identity/v1alpha/participants/" \
 --header 'Content-Type: application/json' \
 --header "x-api-key: $API_KEY" \
 --data "$DATA_CONSUMER"
@@ -115,7 +120,7 @@ DATA_PROVIDER=$(jq -n --arg url "$PROVIDER_CONTROLPLANE_SERVICE_URL" --arg ihurl
            }
        }')
 
-curl --location "http://127.0.0.1/provider/cs/api/identity/v1alpha/participants/" \
+curl --location "http://127.0.0.1:$HOST_HTTP_PORT/provider/cs/api/identity/v1alpha/participants/" \
 --header 'Content-Type: application/json' \
 --header "x-api-key: $API_KEY" \
 --data "$DATA_PROVIDER"
@@ -148,14 +153,14 @@ DATA_ISSUER=$(jq -n --arg pem "$PEM_ISSUER" '{
             }
       }')
 
-curl -s --location 'http://127.0.0.1/issuer/cs/api/identity/v1alpha/participants/' \
+curl -s --location 'http://127.0.0.1:$HOST_HTTP_PORT/issuer/cs/api/identity/v1alpha/participants/' \
 --header 'Content-Type: application/json' \
 --data "$DATA_ISSUER"
 
 ## Seed participant data to the issuer service
 newman run \
   --folder "Seed Issuer SQL" \
-  --env-var "ISSUER_ADMIN_URL=http://127.0.0.1/issuer/ad" \
+  --env-var "ISSUER_ADMIN_URL=http://127.0.0.1:$HOST_HTTP_PORT/issuer/ad" \
   --env-var "CONSUMER_ID=did:web:consumer-identityhub%3A7083:consumer" \
   --env-var "CONSUMER_NAME=MVD Consumer Participant" \
   --env-var "PROVIDER_ID=did:web:provider-identityhub%3A7083:provider" \
